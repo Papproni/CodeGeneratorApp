f35191ee6a5963bf0512b425a0fa9fa516422c77
Papproni <108925289+Papproni@users.noreply.github.com>
2024-11-20 22:09:17 +0100

    started code generation dev

--- fx_builder.py
+++ fx_builder.py
@@ -16,25 +16,18 @@ class bcolors:
 
 class SAB_fx_builder:
     def __init__(self):
-        self.generated_libs_counter = 0 
-        total_length = 35
+        self.template_fx_src = os.path.join(os.path.dirname(__file__),"/customfx_templates/effect_src_template.jinja")
+        self.template_fx_inc = os.path.join(os.path.dirname(__file__),"/customfx_templates/effect_inc_template.jinja")
+        self.block_template_path = os.path.join(os.path.dirname(__file__),"gui_blocks")
+        self.templates_header, self.templates_init, self.templates_process = self.collect_template_files(self.block_template_path)
 
-        # 
-        self.path = {}
+        
 
     def collect_template_files(self,base_path):
-        """
-        Collects and organizes template file paths into dictionaries based on file types.
-
-        Parameters:
-        base_path (str): The root directory path containing the 'jinja_templates' folder.
-
-        Returns:
-        tuple: Three dictionaries (`templates_header`, `templates_init`, `templates_process`) with file identifiers as keys and file paths as values.
-        """
         templates_header = {}
         templates_init = {}
         templates_process = {}
+        missing_templates = []
         
         jinja_templates_path = os.path.join(base_path, 'jinja_templates')
         if not os.path.exists(jinja_templates_path):
@@ -47,21 +40,26 @@ class SAB_fx_builder:
         for subfolder in subfolders:
             subfolder_path = os.path.join(jinja_templates_path, subfolder)
             
-            for sub_subfolder, templates_dict in [
-                ('header', templates_header),
-                ('init', templates_init),
-                ('process', templates_process)
+            # Collect templates from each subfolder
+            current_templates = {"header": None, "init": None, "process": None}
+            for sub_subfolder, templates_dict, key in [
+                ('header', templates_header, "header"),
+                ('init', templates_init, "init"),
+                ('process', templates_process, "process")
             ]:
                 sub_subfolder_path = os.path.join(subfolder_path, sub_subfolder)
                 if os.path.exists(sub_subfolder_path):
                     for file_name in os.listdir(sub_subfolder_path):
                         if file_name.endswith(".jinja"):
                             # Extract the identifier (e.g., "BIQUAD_FILTER")
-                            file_identifier = file_name.split('_template_')[0]
+                            file_identifier = file_name.split('_template_')[0].lower()
                             file_path = os.path.join(sub_subfolder_path, file_name)
                             templates_dict[file_identifier] = file_path
-        
+                            current_templates[key] = file_identifier
         return templates_header, templates_init, templates_process
+    
+    def generate_code(self):
+
 
 
 if __name__ == "__main__":
@@ -69,7 +67,7 @@ if __name__ == "__main__":
     # Example usage
 
     base_path = os.path.join(os.path.dirname(__file__),"gui_blocks")
-    templates_header, templates_init, templates_process = builder.collect_template_files(base_path)
+    templates_header, templates_init, templates_process= builder.collect_template_files(base_path)
     print(templates_header)
     print(templates_init)
     print(templates_process)
